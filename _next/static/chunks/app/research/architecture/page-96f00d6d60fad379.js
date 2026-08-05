e74ec4f3566339b16ae58db2041491c8ce5766b1
Zizhao Hu <zizhaohu3@gmail.com>
2026-08-05 00:49:02 -0700

    Deploy site (personal-site@7ce1dd4)

---
+++ _next/static/chunks/app/research/architecture/page-96f00d6d60fad379.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2897,171,2895,5905,6877,7564],{63521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
